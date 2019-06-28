d7172cd93cae18e71b9180fafb018de01efda7b7
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2019-06-28 11:08:56 -0400

    Initial commit

---
+++ R/jobs.R
@@ -0,0 +1,59 @@
+#' Run Scripts and RStudio Jobs with Pushbullet alerts
+#'
+#' @description
+#' Functions, mostly to be used with RStudio Add-Ins, to run  scripts
+#' with Pushbullet alerts. `source_` functions source in current session,
+#' `bg_` functions in background session.  `_current_` functions use the current
+#' RStudio editor pane, other functions take a script argument.
+#'
+#' these functions will issue a push alert using [RPushbullet::pbPost()].
+#' See [here](https://github.com/eddelbuettel/rpushbullet#initialization) about
+#' setting up Pushbullet with R - you just need to get an API key and run
+#' [RPushbullet::pbSetup()].
+#'
+#' @param script_path The script to run. If NULL for `job_and_pb()` and
+#'   `source_and_pb()`, prompts for a file interactively in the RStudio API.
+#' @param ... passed on to [source()]
+#' @export
+#' @export
+#' @rdname jobs
+source_and_pb <- function(script_path = NULL, ...) {
+  if (is.null(script_path)) script_path <- rstudioapi::selectFile()
+  jobname = paste("R Job:", basename(script_path))
+  withCallingHandlers(
+    source(normalizePath(script_path), ...),
+    error = function(e) {
+      RPushbullet::pbPost("note", jobname, as.character(e))
+      stop(e)
+    }
+  )
+  RPushbullet::pbPost("note", jobname, "This job has finished!")
+}
+
+#' @export
+#' @rdname jobs
+source_current_and_pb <- function(...) {
+  script_path <- rstudioapi::getSourceEditorContext()$path
+  source_and_pb(script_path, ...)
+}
+
+#' @export
+#' @rdname jobs
+bg_and_pb <- function(script_path = NULL) {
+  if (is.null(script_path)) script_path <- rstudioapi::selectFile()
+  callr::r_bg(
+    function(z) pbjob::source_and_pb(z),
+    args = list(z = normalizePath(script_path)),
+    wd = getwd(),
+    system_profile = TRUE, user_profile = TRUE,
+    supervise = TRUE,
+    error = "error"
+  )
+}
+
+#' @export
+#' @rdname jobs
+bg_current_and_pb <- function() {
+  script_path <- rstudioapi::getSourceEditorContext()$path
+  bg_and_pb(script_path = script_path)
+}
---
+++ README.md
@@ -0,0 +1,45 @@
+# pbjob
+
+[![License:
+MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+[![Project Status: WIP – Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.](https://www.repostatus.org/badges/latest/wip.svg)](https://www.repostatus.org/#wip)
+
+
+Run R scripts with Pushbullet Alerts
+
+## Installation
+
+``` r
+remotes::install_github("noamross/pbjob")
+```
+
+## Setup
+
+To use this package you need to use [Pushbullet](https://www.pushbullet.com/) on
+your mobile device or browser and to set up the
+[**RPushbullet**](https://github.com/eddelbuettel/rpushbullet) package. Details
+in that package README, but in short, do this just once:
+
+- Sign up for Pushbullet and install it on your devices/browsers if you haven't
+  already.
+- Log in to <https://www.pushbullet.com/#settings>, click on "Create Access
+  Token", and get the token.
+- Run `RPushbullet::pbSetup(<YOUR_ACCESS_TOKEN>)` to create a config file and
+  select your default devices to send messages to.
+- Restart R
+
+##  Functions
+
+The package has four functions, each of which has an accompanying RStudio Add-In
+
+- `source_and pb(script_path)` runs a script and sends a Pushbullet alert when
+  it is complete.  An alert is also sent if the script errors. If `script_path`
+  is `NULL`, the RStudio interface is used to select a file.
+- `bg_and_pb()` runs the script in a background session with the same alerts.
+  It returns an [`r_process`](https://callr.r-lib.org/reference/r_bg.html) object.
+- `source_current_and_pb()` and `bg_current_and_pb()` do the same but run
+  the current active file in the RStudio editor.
+
+Please note that the 'pbjob' project is released with a
+  [Contributor Code of Conduct](CODE_OF_CONDUCT.md).
+  By contributing to this project, you agree to abide by its terms.
